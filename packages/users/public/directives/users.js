af16d460c30f04ec6b630ff1cea40eb382bb9c19
Benoit Guina <benoit.guina@gmail.com>
2015-04-16 14:31:58 +0200

    users.js directives

---
+++ packages/users/public/directives/users.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('mean.users').directive('userExists', function ($q, $timeout, $http) {
+  return {
+    require:'ngModel',
+    restrict:'A',
+    link: function (scope, el, attrs, ctrl) {
+      ctrl.$asyncValidators.userExists = function(value) {
+        console.log("users directive");
+	if (ctrl.$isEmpty(value)) return $q.when();
+	var def = $q.defer();
+	$http.get('/users/' + value)
+	  .success(function() {
+	    // User does exist
+	    ctrl.$setValidity('userExists', true);
+	    def.resolve();
+	  })
+	  .error(function() {
+	    ctrl.$setValidity('userExists', false);
+	    def.reject();
+	  });
+
+	return def.promise();
+      };
+    }
+  };
+});
